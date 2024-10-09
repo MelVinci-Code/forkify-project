59bf755e0afc915678b67618d9899d2ab9c68df3
MelVinCi-Code <ukaibemelvin@gmail.com>
2024-10-09 22:56:45 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6f53aba7-c1f6-4d00-9435-c6f48304ea99';
+export const MODAL_CLOSE_SEC = 2.5;
